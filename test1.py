ad56ad5edcc128c7b42bab4d3fc8f85bb07f8301
Mahdi-abbasii <work.mahdiabbasi@gmail.com>
2025-09-28 22:31:08 +0330

    11

--- test1.py
+++ test1.py
@@ -1,1 +1,17 @@
-print ("hi mahdi")
+def MyList():
+    Taskmanager=[]
+    return Taskmanager
+def InputUser(Taskmanager):
+    UserInput=input("what is task")
+    Taskmanager.append(UserInput)
+    print (Taskmanager)
+
+def     
+    
+    
+    
+    
+    
+Taskmanager=MyList()
+InputUser(Taskmanager)
+    
